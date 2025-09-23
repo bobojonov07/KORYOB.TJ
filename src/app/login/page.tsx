eb38ba8daef0e56cb47727cddfb70bb09167eb1c
bobojonov07 <bobojonovaminjon2@gmail.com>
2025-09-24 00:15:12 +0300

    Create page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+export default function LoginPage() {
+    return (
+        <div className="flex items-center justify-center min-h-full py-12 px-4">
+            <LoginForm />
+        </div>
+    );
+}
